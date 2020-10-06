889d453ac7e7916a8d5d93550b4554cec92b6b87
Danila Arzhanenkov <arzhan96@gmail.com>
2020-10-06 15:16:56 +0400

    add build too github pages

---
+++ build/precache-manifest.43a87de952149865b0dae39cb16b061b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9437359c6431ef1e5520cd9e4c2b6c4",
+    "url": "/danila-arzhanenkov/build/index.html"
+  },
+  {
+    "revision": "7d168cba7c626900f459",
+    "url": "/danila-arzhanenkov/build/static/css/main.dbfd6a09.chunk.css"
+  },
+  {
+    "revision": "ec9a6e8bf45b17b0b34c",
+    "url": "/danila-arzhanenkov/build/static/js/2.b40e6fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/danila-arzhanenkov/build/static/js/2.b40e6fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d168cba7c626900f459",
+    "url": "/danila-arzhanenkov/build/static/js/main.76cce014.chunk.js"
+  },
+  {
+    "revision": "d9fb67ed06d93455ff61",
+    "url": "/danila-arzhanenkov/build/static/js/runtime-main.82464a52.js"
+  },
+  {
+    "revision": "0a80076b093682cc02d067177068bcd8",
+    "url": "/danila-arzhanenkov/build/static/media/Spinner.0a80076b.gif"
+  },
+  {
+    "revision": "aa5bfc793e8297bd19a3e25754bb6f49",
+    "url": "/danila-arzhanenkov/build/static/media/cloudy.aa5bfc79.svg"
+  },
+  {
+    "revision": "33118352ac5be4f2195bf0efb740d7ce",
+    "url": "/danila-arzhanenkov/build/static/media/day_clear.33118352.svg"
+  },
+  {
+    "revision": "f3e63c912debef85514add0c149e12cc",
+    "url": "/danila-arzhanenkov/build/static/media/day_rain.f3e63c91.svg"
+  },
+  {
+    "revision": "5f54ba40ccc07519a4dac89a2f4e6624",
+    "url": "/danila-arzhanenkov/build/static/media/snow.5f54ba40.svg"
+  },
+  {
+    "revision": "c9ff266199ff33d08972ac48446079fb",
+    "url": "/danila-arzhanenkov/build/static/media/thermometer.c9ff2661.svg"
+  }
+]);
